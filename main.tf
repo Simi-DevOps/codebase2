9c1a7b780947952764b405538ad8a76f47a07193
Simi-DevOps <simiosiefa@gmail.com>
2023-05-31 11:47:42 +0100

    first terraform commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "ec2_AmazonLinux" {
+
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  key_name      = var.ec2_key_name
+
+  security_groups   = [aws_security_group.ec2_sg.name]
+  availability_zone = "eu-west-1a"
+  user_data         = <<EOF
+#!/bin/bash
+yum install java-1.8.0-amazon-corretto-devel.x86_64 maven git -y
+EOF
+  tags              = var.default_tags
+}
---
+++ securitygroups.tf
@@ -0,0 +1,35 @@
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_http_SSH"
+  description = "allow HTTP and SSH inb traffic"
+  vpc_id      = "vpc-0bb955ba49115e69b"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
+  }
+}
+
